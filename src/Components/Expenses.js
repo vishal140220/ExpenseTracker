e75f5ccf97ed04cf810b4cd79c9442eef435c89b
Vishal <p.vishal140220@gmail.com>
2022-01-25 21:36:16 +0530

    date fix

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
-	const [filteredYear, setFilteredYear] = useState("2021");
+	const [filteredYear, setFilteredYear] = useState("2022");
 	const filterChangeHandler = (selectedYear) => {
 		setFilteredYear(selectedYear);
 		console.log("expenses.js " + selectedYear);
@@ -13,7 +13,7 @@ function Expenses(props) {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 	console.log(filteredExpenses);
-	let ExpenseContent = <p>No expenses found.</p>;
+	let ExpenseContent = <p className="no-expenses">No expenses found.</p>;
 	if (filteredExpenses.length > 0) {
 		ExpenseContent = filteredExpenses.map((expense) => (
 			<ExpenseItem
